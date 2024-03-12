90d1556d17c95bbf257168bf589af92d438ac8c0
soubj <sobujahmed124@gmail.com>
2024-03-12 17:13:08 +0600

    added Articles and news

---
+++ src/pages/Blogs/Articles&News/News.jsx
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const News = ({ allBlogs }) => {
+  return (
+    <div className="my-24">
+      <h2 className="font-dm-serif text-4xl md:text-5xl mb-10">
+        Articles & News
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {allBlogs.map((blog) => (
+          <div key={blog._id}>
+            <img
+              src={blog.image}
+              alt="img"
+              className="h-[400px] rounded-t-3xl object-cover w-full"
+            />
+            <h2 className="font-dm-serif text-2xl mt-2">{blog.title}</h2>
+            <div className="flex items-center justify-between mt-5">
+              <p>{new Date(blog?.createdAt).toLocaleDateString()}</p>
+              <Link
+                to={`/blog/${blog._id}`}
+                className="w-9 h-9 rounded-full bg-slate-200 flex items-center justify-center font-semibold">
+                &gt;
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+News.propTypes = {
+  allBlogs: PropTypes.array,
+};
+
+export default News;
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -2,23 +2,37 @@ import { useQuery } from "@tanstack/react-query";
 import MiniHero from "../../components/MiniHero/MiniHero";
 import LatestPost from "./LatestPost/LatestPost";
 import axios from "axios";
+import Spinner from "../../components/Spinner/Spinner";
+import News from "./Articles&News/News";
 
 const Blogs = () => {
   const url =
     "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-  const { data: allBlogs = [] } = useQuery({
+  const {
+    data: allBlogs = [],
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["Articles"],
     queryFn: async () => {
       const res = await axios.get("http://localhost:3000/api/v1/blogs");
       return res.data.data;
     },
   });
-  console.log("data:", allBlogs);
+
   return (
     <>
       <MiniHero text="Articles & News" img={url} />
       <LatestPost />
+
+      {isLoading ? (
+        <Spinner />
+      ) : isError ? (
+        <p className="text-center my-5">Something went wrong</p>
+      ) : (
+        <News allBlogs={allBlogs} />
+      )}
     </>
   );
 };
